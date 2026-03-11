625afe256722db03b4bea352d808d9a06baa1f89
sopuruchukwuanthony <uboguanthonysopuruchukwu@gmail.com>
2026-03-11 11:21:19 +0100

    feayure: added arays

--- array.cs
+++ array.cs
@@ -4,27 +4,29 @@
 
 // int [] numbers = {10, 20, 30, 40, 50, 60};
 // Console.WriteLine(numbers.Max()); // Output: 60
-int[] numbers = { 5, 10, 30, 70, 90 }; // sorted
+// int[] numbers = { 5, 10, 30, 70, 90 }; // sorted
 
-int target = 70;
-int left = 0;
-int right = numbers.Length - 1;
+// int target = 70;
+// int left = 0;
+// int right = numbers.Length - 1;
 
-while (left <= right)
-{
-    int mid = (left + right) / 2;
+// while (left <= right)
+// {
+//     int mid = (left + right) / 2;
 
-    if (numbers[mid] == target)
-    {
-        Console.WriteLine("Found at index " + mid);
-        break;
-    }
-    else if (numbers[mid] < target)
-    {
-        left = mid + 1;
-    }
-    else
-    {
-        right = mid - 1;
-    }
-}
+//     if (numbers[mid] == target)
+//     {
+//         Console.WriteLine("Found at index " + mid);
+//         break;
+//     }
+//     else if (numbers[mid] < target)
+//     {
+//         left = mid + 1;
+//     }
+//     else
+//     {
+//         right = mid - 1;
+//     }
+// }
+string[] names = { "Alice", "Bob", "Charlie", "David", "Eve" };
+Console.WriteLine(string.Join(", ", names));
---
+++ list.cs
@@ -0,0 +1,10 @@
+List<string> fruits = new List<string>{
+    "Apple",
+    "Banana",
+    "Cherry"
+};
+Console.WriteLine("Fruits:");
+foreach (string fruit in fruits)
+{
+    Console.WriteLine(fruit);
+}
