827e8da58877f4d2d9888a6b0f2c26d7ae86e524
sopuruchukwuanthony <uboguanthonysopuruchukwu@gmail.com>
2026-05-16 00:17:12 +0100

    new Feature

---
+++ loops.cs
@@ -0,0 +1,26 @@
+// Console.WriteLine("what do you want to write");
+// String message = Console.ReadLine();
+
+// Console.Write("how many time do you want to write it: ");
+// int counter = Convert.ToInt32(Console.ReadLine());
+
+
+// if(counter <= 0)
+// {
+//     Console.WriteLine("enter a valid number");
+// }
+// for(int i = 0; i < counter; i++)
+// {
+//     Console.WriteLine(message);
+// }
+//  int i = 0;
+
+//  while(i < 10)
+// {
+//     i++;
+//     Console.WriteLine(i);
+// }
+
+int age = 9;
+
+Console.WriteLine(age >= 10 ? "valid" : "invalid");
